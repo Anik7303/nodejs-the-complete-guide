957104289d4428c257bedc3b5c38ab0e371fadbf
Anik7303 <anik7303@gmail.com>
2020-04-20 02:17:26 +0600

    retyping section 28 progress

---
+++ Retry/Backend/graphql/schema.js
@@ -0,0 +1,66 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    type AuthData {
+        token: String!
+        userId: String!
+    }
+
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
+
+    input UserInputData {
+        name: String!
+        email: String!
+        password: String!
+    }
+
+    input LoginInputData {
+        email: String!
+        password: String!
+    }
+
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
+    }
+
+    type RootQuery {
+        hello: String!
+        login(loginInput: LoginInputData): AuthData!
+        posts: PostData!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
